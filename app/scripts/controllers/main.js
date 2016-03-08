0a49114997bddc6277b590c0d68eb415d0655e05
jpk24rent <jarkko@24rent.fi>
2016-03-08 18:33:29 +0200

    skeleton

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the volontarioWebApp
  */
 angular.module('volontarioWebApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+	
+  $scope.lat ='60.173324418807134';
+  $scope.long = '24.941024799999923';
+  var myLatlng = new google.maps.LatLng($scope.lat, $scope.long);
+
+  function initialize() {
+        var mapOptions  = {
+          center: myLatlng,
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          disableDefaultUI: true,
+          scrollwheel: false,
+        };
+        var map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+        $scope.map = map;
+      }
+      initialize();
   });
